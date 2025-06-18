577c82b4160b4bce69dde822e68462b99d605c5a
liaofan <weifengjiang1989@gmail.com>
2025-06-19 00:29:36 +0800

    chore: 38

---
+++ gtest_proj/src/38_permute.cc
@@ -0,0 +1,18 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <vector>
+
+// https://leetcode.cn/problems/permutations/
+// 题目：给定一个不含重复数字的数组 nums，返回其所有可能的全排列。你可以按任意顺序返回答案。
+class Solution38 {
+public:
+    std::vector<std::vector<int>> permute(const std::vector<int>& nums) {
+        std::vector<std::vector<int>> res;
+
+        return res;
+    }
+};
+
+TEST(ut_38, permute) {
+}
